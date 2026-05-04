205f6dd7523cccf72c6b188ad226b834dd96ba5e
Borges <borgespaulo72@yahoo.com.br>
2026-05-04 18:38:22 -0300

    feat: Criando endpoint pra testar despesa

---
+++ src/CashFlowTwo.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using CashFlowTwo.Communication.Request;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlowTwo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+
+        public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+        {
+            return Created();
+        }
+    }
+}
---
+++ src/CashFlowTwo.Communication/Request/RequestRegisterExpenseJson.cs
@@ -0,0 +1,17 @@
+﻿using CashFlowTwo.Communication.Enums;
+
+namespace CashFlowTwo.Communication.Request
+{
+    public class RequestRegisterExpenseJson
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime Date { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public PaymentType PaymentType { get; set; }
+    }
+}
